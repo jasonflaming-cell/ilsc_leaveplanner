31b5650a63f5e61a14010522f12a91ebc424a138
jasonflaming-cell <jason.flaming@gmail.com>
2025-08-13 18:40:12 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',      // <— important: serve assets relative to index.html
+  base: './',      // <<< important: load /assets relative to index.html
 })
